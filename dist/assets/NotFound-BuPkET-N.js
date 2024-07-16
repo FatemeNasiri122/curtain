fca2b42cb8aa68b25fac72d7ad1f6149bc79f939
nasiri <futinasiri7@gmail.com>
2024-07-16 21:01:40 +0330

    fix

--- dist/assets/NotFound-BuPkET-N.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-B8ZINai6.js";const r=()=>o.jsx("div",{children:"NotFound"});export{r as default};
--- src/data/product.js
+++ src/data/product.js
@@ -3,16 +3,16 @@ const item = {
     name: "linen sheer grommet drapery",
     price: "115,000",
     images: [
-        { id: 1, img: "../public/assests/images/european-flax-linen-curtain-xldarkflax7.jpg" },
-        { id: 2, img: "../public/assests/images/img7x-darkflax.jpg" },
-        { id: 3, img: "../public/assests/images/img7x-darkflax3.jpg" },
-        { id: 4, img: "../public/assests/images/img7x-darkflax4.jpg" },
+        { id: 1, img: "/assets/images/european-flax-linen-curtain-xldarkflax7.jpg" },
+        { id: 2, img: "/assets/images/img7x-darkflax.jpg" },
+        { id: 3, img: "/assets/images/img7x-darkflax3.jpg" },
+        { id: 4, img: "/assets/images/img7x-darkflax4.jpg" },
     ],
     colors: [
-        { id: 1, name: "white", img: "../public/assests/images/european-flax-linen-curtain-xldarkflax7.jpg" },
-        { id: 2, name: "white", img: "../public/assests/images/img7x-darkflax.jpg" },
-        { id: 3, name: "white", img: "../public/assests/images/img7x-darkflax3.jpg" },
-        { id: 4, name: "white", img: "../public/assests/images/img7x-darkflax4.jpg" },
+        { id: 1, name: "white", img: "/assets/images/european-flax-linen-curtain-xldarkflax7.jpg" },
+        { id: 2, name: "white", img: "/assets/images/img7x-darkflax.jpg" },
+        { id: 3, name: "white", img: "/assets/images/img7x-darkflax3.jpg" },
+        { id: 4, name: "white", img: "/assets/images/img7x-darkflax4.jpg" },
     ],
     sizes: [
         { id: 1, name: "140CM 220CM", available: false, },
@@ -25,13 +25,13 @@ const item = {
         { id: 2, name: "unlined" },
     ],
     quantity: 1,
-    selectedColor: { id: 1, name: "white", img: "../public/assests/images/european-flax-linen-curtain-xldarkflax7.jpg" },
+    selectedColor: { id: 1, name: "white", img: "/assets/images/european-flax-linen-curtain-xldarkflax7.jpg" },
     selectedSize: { id: 3, name: "280CM 300CM", available: false, },
-    selectedImage: { id: 1, img: "../public/assests/images/european-flax-linen-curtain-xldarkflax7.jpg" },
-    discription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum nemo ab minus ea et nam, doloremque delectus praesentium quibusdam reiciendis accusantium! Laudantium, sint dicta itaque sunt distinctio soluta sapiente.     Lorem ipsum dolor sit amet consectetur adipisicin elit.Nulla  ipsum nemo ab minus ea et nam, doloremque delectus praesentium quibusdam reiciendis accusantium! Laudantium, sint dicta itaque sunt distinctio soluta sapiente.",
+    selectedImage: { id: 1, img: "/assets/images/european-flax-linen-curtain-xldarkflax7.jpg" },
+    discription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum nemo ab minus ea et nam, doloremque delectus praesentium quibusdam reiciendis accusantium! Laudantium, sint dicta itaque sunt distinctio soluta sapiente.     Lorem ipsum dolor sit amet consectetur adipisicing elit.Nulla ipsum nemo ab minus ea et nam, doloremque delectus praesentium quibusdam reiciendis accusantium! Laudantium, sint dicta itaque sunt distinctio soluta sapiente.",
     dimensions: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum nemo ab minus ea et nam, doloremque delectus praesentium quibusdam reiciendis accusantium! Laudantium, sint dicta itaque sunt distinctio soluta sapiente.",
     materialAndCare: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum nemo ab minus ea et nam, doloremque delectus praesentium quibusdam reiciendis accusantium! Laudantium, sint dicta itaque sunt distinctio soluta sapiente.",
-    shippingsAndReturn: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum nemo ab minus ea et nam,.",
+    shippingsAndReturn: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum nemo ab minus ea et nam, doloremque delectus praesentium quibusdam reiciendis accusantium! Laudantium, sint dicta itaque sunt distinctio soluta sapiente.",
 }
 
 export { item };
